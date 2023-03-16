e490d8f2f98abc184c2cd5588eda6444189a96e9
AbPip <117826794+AbPip@users.noreply.github.com>
2023-03-16 21:13:10 +0000

    Defined days+months

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 let now = new Date();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
