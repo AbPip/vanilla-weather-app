2d41add046b9a6fae921a277758081765124f240
AbPip <117826794+AbPip@users.noreply.github.com>
2023-03-21 14:50:00 +0000

    InnerHTML of temperature display linked to weather data

--- src/index.js
+++ src/index.js
@@ -36,3 +36,14 @@ if (minute < 10) {
 let date = now.getDate();
 let tellingTime = document.querySelector(".tellingTime");
 tellingTime.innerHTML = `${day}, ${hour}:${minute}`;
+
+function displayTemperature(response) {
+  let tempInteger = document.querySelector(".degree");
+  tempInteger.innerHTML = Math.round(response.data.main.temp);
+}
+
+let apiKey = `000e705a9dacee472a121f7e5978d1ca`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Wellington&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
