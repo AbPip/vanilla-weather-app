6c69cd93f749d42d5683d616a986af51916b4db1
AbPip <117826794+AbPip@users.noreply.github.com>
2023-04-09 14:10:25 +0100

    Smartening up forecast
    Created loop for forecast to be more intelligent/avoiding repeated code.
    Removed all content from div (id=forecast) and interpolated into JS function

--- src/index.js
+++ src/index.js
@@ -24,11 +24,13 @@ function formatDate(timestamp) {
 function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
 
-  forecastElement.innerHTML = `
-  <div class="weather-forecast" id="forecast">
-    <div class="row">
-      <div class="col-2">
-        <div class="weather-forecase-date">Thu</div>
+  let forecastHTML = `<div class="row">`;
+  let days = ["Sun", "Mon", "Tues", "Weds"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+        <div class="weather-forecase-date">${day}</div>
           <img
           src="https://openweathermap.org/img/wn/04n@2x.png"
            width="40"
@@ -37,9 +39,11 @@ function displayForecast() {
               <span class="weather-forecast-temperature-max">18°</span>
               <span class="weather-forecast-temperature-min">12°</span>
           </div>
-        </div>
-     </div>
-   </div>`;
+        </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 function displayTemperature(response) {
